118fe4473dcf234ca7a601265be929e64390a5b8
Hackpaul <fpaulraj2007@gmail.com>
2026-07-15 21:49:48 +0530

    add : shared header

---
+++ shared.h
@@ -0,0 +1,19 @@
+#ifndef GETLINE
+#define GETLINE
+
+struct line{             //Shared structure for string buffers
+
+	int code;
+	char *buffer;
+	size_t buffer_size;
+        char *tokens[10];
+};
+
+void get_line(struct line *ptr);
+int parser(struct line *ptr);
+void execute_command(struct line *ptr);
+void parent(pid_t pid);
+
+#endif 
+
+
