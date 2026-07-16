07b9b09b0757fd1f31f1ab5b6b86fd504d55db26
Hackpaul <fpaulraj2007@gmail.com>
2026-07-16 21:19:33 +0530

    Add : make child process exit on wrong command

---
+++ pid_handler.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+#include "shared.h"
+
+void execute_command(struct line *buffer){
+	execvp(buffer->tokens[0],buffer->tokens);
+	printf("shell :%s :is not recognised as a command\n",buffer->tokens[0]);
+	exit(EXIT_FAILURE);
+}
+
+void parent(pid_t child_pid){
+	waitpid(child_pid,NULL,0);
+}
+
