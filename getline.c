a2201281d9b1271c9955f0828402b9e2782cfbec
Hackpaul <fpaulraj2007@gmail.com>
2026-07-20 20:35:18 +0530

    change : add spaces for clean look

--- getline.c
+++ getline.c
@@ -5,24 +5,24 @@
 
 void get_line(struct line *ptr){
 
-    char *line=NULL;
+    char *line = NULL;
     size_t buffer_size = 0;
     ssize_t n_size;
     
     n_size=getline(&line,&buffer_size,stdin);
-        if(n_size!=-1){		
-        line[strcspn(line,"\n")]='\0'; 		
-        ptr->buffer=line;
-	ptr->code=1;
-	ptr->buffer_size=n_size;
+        if(n_size != -1){		
+        line[strcspn(line,"\n")] = '\0'; 		
+        ptr->buffer = line;
+	ptr->code = 1;
+	ptr->buffer_size = n_size;
     } else if(feof(stdin)){    //Check for EOF
-        ptr->buffer=line;
-	ptr->code=0;
-	ptr->buffer_size=0;
+        ptr->buffer = line;
+	ptr->code = 0;
+	ptr->buffer_size = 0;
     } else if(ferror(stdin)){  //Check for system error
-        ptr->code=-1;
-	ptr->buffer=line;
-	ptr->buffer_size=0;
+        ptr->code = -1;
+	ptr->buffer = line;
+	ptr->buffer_size = 0;
     }
 
 }
