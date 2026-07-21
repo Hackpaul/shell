406ada55cbdda788929915a9ca02e424f97be812
Hackpaul <fpaulraj2007@gmail.com>
2026-07-21 22:03:08 +0530

    add : free the linked list

--- buildins.c
+++ buildins.c
@@ -3,44 +3,51 @@
 #include <string.h>
 #include "shared.h"
 
-
-void initialize_buildins(struct hash_table *ptr){
- //   unsigned char *temp = malloc(sizeof(char[10])); // to add a new function , add it before NULL. 
- //     memset(temp,0,10);
- //   snprintf((char *)temp,10,"exit");
-    ptr->buildins[0] = (unsigned char *)"exit";
-    ptr->buildins[1] = (unsigned char *)"cd";
-    ptr->buildins[2] = NULL;
-    hash(ptr);
-}
-
-int buildin_handler(struct line* buffer , struct hash_table *ptr){
-    int i = 0 ,is_found = FAIL;
-    unsigned long string_hash = hash_string((unsigned char *)buffer->tokens[0]);
-    
-    while(i < MAX_BUILDINS_COMMANDS){
-        if(ptr->hash_array[i] != 0){
-            if(string_hash == ptr->hash_array[i]){
-            is_found = SUCCESS;
-
-	    break;
-            }
-            i++;
-    	}
-	else {
-            break;
+void free_nodes(struct hash_table *ptr){
+    struct string_hash *back, *front;
+    int i;
+    for(i=0; i<HASH_TABLE; i++){
+        front = ptr->hash_array[i];
+        while(front != NULL){
+            back = front ;
+	    front = front->next;
+	    free(back);
 	}
     }
 
-    if(is_found){
+}
 
-        if(i == 0){         // exit - command
+void initialize_buildins(struct hash_table *ptr){
+                                 // to add a new function , add it before NULL. 
+    ptr->buildins[0] = "exit";
+    ptr->buildins[1] = "cd";
+    ptr->buildins[2] = NULL;
+    hash_buildins(ptr);
+}
+
+int buildin_handler(struct line* buffer , struct hash_table *ptr){
+    int is_found = FAIL;
+    unsigned long hash = hash_string(buffer->tokens[0]);
+    struct string_hash *temp;
+    temp = ptr->hash_array[hash];
+    while(temp != NULL){
+        if(strcmp(temp->string,buffer->tokens[0]) == 0){
+            is_found = SUCCESS;
+	    break;
+	}
+	temp = temp->next;
+    }
+
+    if(is_found == SUCCESS){
+
+        if(hash == ptr->hashed_values[0]){         // exit - command
             fflush(stdout);
 	    free(buffer->buffer);
+	    free_nodes(ptr);
 	    exit(SUCCESS);
         }
 
-        if(i == 1){
+        if(hash == ptr->hashed_values[1]){
             printf("cd cmd!\n");
         }
 
