dccdc8fdb9ce30e9dbc533595783f1d2ae503e79
Hackpaul <fpaulraj2007@gmail.com>
2026-07-21 22:04:23 +0530

    feat : add sizeof string as hash algorithm

--- hash.c
+++ hash.c
@@ -2,27 +2,42 @@
 #include <stdlib.h>
 #include "shared.h"
 
-unsigned long hash_string(unsigned char *str) {
-    unsigned long hash = 5381;
-    int c;
-
-    // Loop through every character until the null terminator \0
-    while ((c = *str++)) {
-        // hash * 33 + c
-        hash = ((hash << 5) + hash) + c; 
-    }
-
+unsigned long hash_string(char *string) {
+    unsigned long hash = 0;
+    while(string[hash]!= '\0'){
+        hash ++;
+    }	
     return hash;
 }
 
 
-int hash(struct hash_table *ptr){
-    int no_of_hashed = 0;
+int hash_buildins(struct hash_table *ptr){
+    int count = 0;
+    unsigned long hash;
+    struct string_hash *temp;
+    while(ptr->buildins[count] != NULL){
+        hash = hash_string(ptr->buildins[count]);
+	ptr->hashed_values[count] = hash;
+	temp = ptr->hash_array[hash];
 
-    while(ptr->buildins[no_of_hashed] != NULL){
-    ptr->hash_array[no_of_hashed] = hash_string(ptr->buildins[no_of_hashed]);
-    no_of_hashed ++;
+	if(temp == NULL){                                    // Creation of node
+            temp = malloc(sizeof(struct string_hash));
+	    if(temp == NULL){
+                printf("Malloc failed\n");
+	    }
+	    temp->string = ptr->buildins[count];
+	    ptr->hash_array[hash] = temp;
+	} else {
+            while(temp->next != NULL){	                      // appendation of node 
+	        temp = temp->next;
+
+            }
+	    temp->next = malloc(sizeof(struct string_hash));
+	    temp->next->string = ptr->buildins[count];
+	    temp->next->next = NULL;
+	}
+        count ++;
     }
-    ptr->hash_array[no_of_hashed]=0;
-    return no_of_hashed;
+    return count;
 }
+
