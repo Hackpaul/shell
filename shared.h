758e83feacd66a877a458cab479c3610f60f78b5
Hackpaul <fpaulraj2007@gmail.com>
2026-07-21 22:05:40 +0530

    change : add a linked node

--- shared.h
+++ shared.h
@@ -1,34 +1,42 @@
-#ifndef GETLINE
-#define GETLINE
+#ifndef SHARED
+#define SHARED
 
 #define FAIL 0
 #define SUCCESS 1 
 #define MAX_BUILDINS_COMMANDS 10
 #define MAX_TOKENS 10
+#define HASH_TABLE 10
 
 struct line{             //Shared structure for string buffers
-
 	int code;
 	int no_of_arguments;
 	char *buffer;
 	size_t buffer_size;
         char *tokens[MAX_TOKENS];
+};
 
+struct string_hash {
+    char *string;
+    struct string_hash *next;
 };
 
 struct hash_table{
-    unsigned char *buildins[MAX_BUILDINS_COMMANDS];
-    unsigned long hash_array[MAX_BUILDINS_COMMANDS];
+    char *buildins[MAX_BUILDINS_COMMANDS];
+    struct string_hash *hash_array[HASH_TABLE];
+    unsigned long hashed_values[MAX_BUILDINS_COMMANDS]; 
 };
 
-void get_line(struct line *ptr);
-int parser(struct line *ptr);
-void execute_command(struct line *ptr);
-void parent(pid_t pid);
-void initialize_buildins(struct hash_table *ptr);
-int buildin_handler(struct line *buffer , struct hash_table *ptr);
-int hash(struct hash_table *ptr);
-unsigned long hash_string(unsigned char *str);
+ void get_line(struct line *ptr);
+ int parser(struct line *ptr);
+
+ void execute_command(struct line *ptr);
+ void parent(pid_t pid);
+ void initialize_buildins(struct hash_table *ptr);
+ int buildin_handler(struct line *buffer , struct hash_table *ptr);
+
+ int hash_buildins(struct hash_table *ptr);
+ unsigned long hash_string(char *string);
+ 
 
 #endif 
 
