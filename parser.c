2c0332cfeec7faf4047e639db5943f552b448c39
Hackpaul <fpaulraj2007@gmail.com>
2026-07-24 23:17:41 +0530

    add new headers

--- parser.c
+++ parser.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#include "shared.h"
 
-int parser(struct line *buffer){
+#include "shared.h"
+#include "struct.h"
+#include "function.h"
+
+int parser(input *buffer){
 
 	char *temp = strtok(buffer->buffer," ");
 	int count = 0;
-        while(temp != NULL && count < 10){
+        while(temp != NULL && count < MAX_TOKENS){
 		buffer->tokens[count] = temp;
 		count ++;
 		temp = strtok(NULL," ");
