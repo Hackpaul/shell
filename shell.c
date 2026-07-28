aa200bc44115d6bfa39a35d8782ebb9dd212fd99
Hackpaul <fpaulraj2007@gmail.com>
2026-07-28 21:42:59 +0530

    change : handle signals

--- shell.c
+++ shell.c
@@ -2,14 +2,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <signal.h>
 
 #include "shared.h"
 #include "struct.h"
 #include "function.h"
 
+void sigint_handler(int sig){
+
+        write(STDOUT_FILENO, "\n", 1);
+}
+
 int main(void){
         int count , stop = 1;
         pid_t process_id;
+
+
+	struct sigaction signal;
+	signal.sa_handler = sigint_handler;
+	sigemptyset(&signal.sa_mask);
+	signal.sa_flags = 0;
+	sigaction(SIGINT,&signal,NULL);
         
         input shared_buffer; // Declare a shared structure for buffer
         hash_table hash = {0}; 
@@ -23,6 +36,7 @@ int main(void){
 	    getcwd(path,sizeof(path));
 	    setenv("PWD",path,1);
 	    printf("%s@pc$%s ",getenv("USER"),getenv("PWD"));
+	    fflush(stdout);
 	    get_line(&shared_buffer);    // Pass the struct and get update
 
       	    if(shared_buffer.code == 1){
@@ -42,10 +56,9 @@ int main(void){
 	            }
 	        }
              } else if(shared_buffer.code == 0){
-                 printf("User stopped it!\n");
+                 printf("\n");
 	         stop = 0;
              } else if(shared_buffer.code==-1){
-                 fprintf(stderr,"Error while reading user input!\n");
 	         stop = 1;
 	     }
 	     free(shared_buffer.buffer);
