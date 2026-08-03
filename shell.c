2b5819e9488f0931e46c8b86e37a32bc8d62d0d6
Hackpaul <fpaulraj2007@gmail.com>
2026-08-03 19:31:32 +0530

    change : move signal handling

--- shell.c
+++ shell.c
@@ -2,41 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <signal.h>
-
+	
 #include "shared.h"
 #include "struct.h"
 #include "function.h"
-
-void sigint_handler(int sig){                        //Fucntion : SIGINT  	
-    (void)sig;
-    write(STDOUT_FILENO, "\n", 1);
-}
-
 
 
 int main(void){
 
     int count , stop = 1;
     pid_t process_id;
-
-    struct sigaction sigint , sigtstp , sigquit;      // Signal : SIGINT     
-
-    sigint.sa_handler = sigint_handler;
-    sigemptyset(&sigint.sa_mask);
-    sigint.sa_flags = 0;
-    sigaction(SIGINT,&sigint,NULL);
-    
-    sigemptyset(&sigtstp.sa_mask);                    // Signal : SIGTSTP
-    sigtstp.sa_handler = SIG_IGN;
-    sigaction(SIGTSTP,&sigtstp,NULL);
-
-    sigemptyset(&sigquit.sa_mask);                    // Signal : SIGQUIT
-    sigquit.sa_handler = SIG_IGN;
-    sigaction(SIGQUIT,&sigquit,NULL);
-
+ 
     input shared_buffer;                              // Declare a shared structure for buffer
-
+    initialize_signals();
     hash_table hash = {0};                            // Initialize hash tables struct 
     shared_buffer.table = &hash;  
     char path[PATH_SIZE] = {0};
