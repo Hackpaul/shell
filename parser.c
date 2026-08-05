7bb45735df6043a4d0022a9ec641f819e22433fc
Hackpaul <fpaulraj2007@gmail.com>
2026-08-05 20:25:17 +0530

    remove : roll back

--- parser.c
+++ parser.c
@@ -20,34 +20,3 @@ int parser(input *buffer){
         return count;
 }
 
-/*
-   A custom parser is under progress !
-   Why this choice : 
-
-   	1. To get a full control over whats the user is typing.
-	2. This can be easily extended over each new feats. 
-*//* 
-
-int parser(input *buffer){
-    int count = 0, pos = 0 ,struct = 0 , is_error = FAIL;
-    char *temp = buffer->buffer;
-    while(temp[pos] != '\0'){
-        while(temp[pos] != ' '){
-            if(temp[pos] == '|'){
-                struct ++;
-		if(temp[pos + 1] == '|'){
-                    fprintf(stderr,"shell: syntax error near unexpected token `|' \n");
-		    return 1;
-                }
-            }
-            count ++;
-	}
-        pos ++;	
-	if(is_error == SUCCESS){
-            break;
-        }
-    }
-    return 0;
-}
-*/
-
