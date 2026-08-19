bfb648c826a515f7450c1d68e2e53de9d8d3f83a
Hackpaul <fpaulraj2007@gmail.com>
2026-08-19 22:22:01 +0530

    Change : transition to char pointers as parameter

--- function.h
+++ function.h
@@ -9,23 +9,15 @@ void initialize_signals(void);
 
 
 // Buildin command 
-void do_exit(input *buffer);
-void do_cd(input *buffer);
-
-
+void do_exit(char **tokens,int count,pointer_struct *ptr);
+void do_cd(char **tokens,int count,pointer_struct *ptr);
 
 // Global functions
 void get_line(input *ptr);
-int parser(input *ptr);
 
-void execute_command(input *ptr);
 void parent(pid_t pid);
 void initialize_buildins(hash_table *ptr);
-int buildin_handler(input *buffer , hash_table *ptr);
-
-int hash_buildins(hash_table *ptr);
-unsigned long hash_string(char *string); 
-
-void free_nodes(hash_table *ptr);
+int buildin_handler(char **tokens, int count,pointer_struct *ptr);
+void execute_command(char **tokens);
 
 #endif
