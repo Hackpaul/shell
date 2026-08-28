08e341c3b12f915da0f709e2fb2ed8750c40cd29
Hackpaul <fpaulraj2007@gmail.com>
2026-08-28 16:08:38 +0530

    Change : whole poitners to required hash table pointer

--- buildins.c
+++ buildins.c
@@ -17,19 +17,20 @@ void initialize_buildins(hash_table *ptr){     // to add a new function , add it
     hash_buildins(ptr);
 }
 
-int buildin_handler(char **tokens , int count, pointer_struct *ptr){
+int buildin_handler(char **tokens , int count, hash_table *ptr){
     int is_found = FAIL;
-    hash_table *hash_table_ptr = (hash_table *)ptr->hash_table_ptr;
+    hash_table *hash_table_ptr = ptr;
     unit hash = hash_string(tokens[0]);
     hash_node *temp = hash_table_ptr->hash_array[hash];
 
     while(temp != NULL){
+
         if(strcmp(temp->string,tokens[0]) == 0){
-            is_found = SUCCESS;
-            hash_table_ptr->do_command[temp->slot](tokens ,count ,ptr);
+            is_found = hash_table_ptr->do_command[temp->slot](tokens ,count );
             break;
         }
         temp = temp->next;
+
     }
     return is_found;
 }
