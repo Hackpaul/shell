2a92669451deb4f70037e2942b6f41aca0f06588
Hackpaul <fpaulraj2007@gmail.com>
2026-08-28 11:32:36 +0530

    Remove : seperate cmd exectuer

--- helper.h
+++ helper.h
@@ -6,7 +6,6 @@
 
 struct tree_node *create_node(Node_type type);
 
-void execute_cmds(char **tokens,int count, pointer_struct *ptr);
 int execute_ast(tree_node *head,pointer_struct *ptr);
 int free_node(tree_node *head);
 
