e5b235b8e22808a4a1736a7222d57f244169be71
Hackpaul <fpaulraj2007@gmail.com>
2026-08-28 16:13:35 +0530

    Change : headers for inlined execute cmd

--- function.h
+++ function.h
@@ -9,14 +9,16 @@ void initialize_signals(void);
 
 
 // Buildin command 
-void do_exit(char **tokens,int count,pointer_struct *ptr);
-void do_cd(char **tokens,int count,pointer_struct *ptr);
+int do_exit(char **tokens,int count);
+int do_cd(char **tokens,int count);
 
 // Global functions
 void get_line(input *ptr);
 
+int check_is_redirection(tree_node *ptr);
+
 void initialize_buildins(hash_table *ptr);
-int buildin_handler(char **tokens, int count,pointer_struct *ptr);
+int buildin_handler(char **tokens, int count, hash_table *ptr);
 void execute_command(char **tokens);
 
 #endif
